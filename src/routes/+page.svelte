1e02a9a7586163669490563a5fbfb06c8aa88487
gginglis <inglisge23@craighead.school.nz>
2024-06-30 09:09:38 +1200

    added confirmation alert to load button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,6 +11,24 @@
   function removeBook(index) {
     books = [...books.slice(0, index), ...books.slice(index + 1)];
   }
+  // removes books from the array
+
+  function saveBooks() {
+    localStorage.books = JSON.stringify(books);
+  }
+
+  function loadBooks() {
+    books = JSON.parse(localStorage.books);
+  }
+
+  function confirmLoadBooks() {
+    var userConfirmed = confirm("are you sure");
+    if (userConfirmed) {
+      loadBooks();
+    } else {
+      console.log("cancelled");
+    }
+  }
 </script>
 
 <Header />
@@ -29,6 +47,14 @@
         removeBook(index);
       }}>remove</button
     >
+
+    <button
+      on:click={() => {
+        saveBooks(index);
+      }}>save</button
+    >
+
+    <button on:click={confirmLoadBooks}>load</button>
   {/each}
 </main>
 
