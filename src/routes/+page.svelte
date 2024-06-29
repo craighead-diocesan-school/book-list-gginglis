b157ed00777708336ccc335ff6c83328b55ae98c
gginglis <inglisge23@craighead.school.nz>
2024-06-30 09:18:00 +1200

    added comments to code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,17 +16,22 @@
   function saveBooks() {
     localStorage.books = JSON.stringify(books);
   }
+  // saves books to storage
 
   function loadBooks() {
     books = JSON.parse(localStorage.books);
   }
+  // retrieves saved books after refreshing
 
   function confirmLoadBooks() {
     var userConfirmed = confirm("are you sure");
+    // asks the user whether they want to load or not
     if (userConfirmed) {
       loadBooks();
+      // loads the books at user's confirmation
     } else {
       console.log("cancelled");
+      // cancels the request to load books
     }
   }
 </script>
@@ -41,20 +46,24 @@
     <div class="book">
       <input bind:value={books[index]} />
     </div>
+    <!-- lets the user input a book title -->
 
     <button
       on:click={() => {
         removeBook(index);
       }}>remove</button
     >
+    <!-- removes user's chosen book from the array -->
 
     <button
       on:click={() => {
         saveBooks(index);
       }}>save</button
     >
+    <!-- saves the book to the localStorage -->
 
     <button on:click={confirmLoadBooks}>load</button>
+    <!-- asks the user whether they want to load the books or not, and runs whatever they pick -->
   {/each}
 </main>
 
